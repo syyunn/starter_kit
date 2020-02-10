08e99a407c9672a712da887dc3f2a577f6b58573
zachary <syyun@snu.ac.kr>
2020-02-10 16:16:26 +0900

    Add product data-structure

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,6 +2,16 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint256 public productCount = 0;
+    mapping(uint256 => Product) public products;
+
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 price;
+        address owner;
+        bool purchased;
+    }
 
     constructor() public {
         name = "Dapp University Marketplace";
