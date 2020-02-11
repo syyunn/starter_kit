6ffcd2c294f2fbd8e0d846e3eb139cd715b935c2
zachary <syyun@snu.ac.kr>
2020-02-11 15:10:52 +0900

    Add purchase function to contract

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -21,6 +21,14 @@ contract Marketplace {
         bool purchased
     );
 
+    event ProductPurchased(
+        uint256 id,
+        string name,
+        uint256 price,
+        address payable owner,
+        bool purchased
+    );
+
     constructor() public {
         name = "Dapp University Marketplace";
     }
@@ -42,5 +50,36 @@ contract Marketplace {
         );
         // Trigger an event
         emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    function purchaseProduct(uint256 _id) public payable {
+        // Fetch the product
+        Product memory _product = products[_id];
+        // Fetch the owner
+        address payable _seller = _product.owner;
+        // Make sure the product has a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+        // Require that there is enough Ether in the transaction
+        require(msg.value >= _product.price);
+        // Require that the product has not been purchased already
+        require(!_product.purchased);
+        // Require that the buyer is not the seller
+        require(_seller != msg.sender);
+        // Transfer ownership to the buyer
+        _product.owner = msg.sender;
+        // Mark as purchased
+        _product.purchased = true;
+        // Update the product
+        products[_id] = _product;
+        // Pay the seller by sending them Ether
+        address(_seller).transfer(msg.value);
+        // Trigger an event
+        emit ProductPurchased(
+            productCount,
+            _product.name,
+            _product.price,
+            msg.sender,
+            true
+        );
     }
 }
